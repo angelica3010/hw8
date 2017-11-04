b747913ab24cbb6b58573604fa2863cdce8822b7
Angel <angel@Angels-MacBook-Air.local>
2017-11-03 22:43:31 -0400

    added photos

--- flask_portfolio/portfolio/views.py
+++ flask_portfolio/portfolio/views.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, request
 
 like_count = 0
 
-@app.route('/')
+@app.route('/', methods = ['POST','GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return render_template('photos.html')
+    else:
+        return render_template('index.html')
 
 
 @app.route('/about', methods = ['POST','GET'])
@@ -15,6 +18,10 @@ def about():
         global like_count
         like_count += 1
         print(f'count is now {like_count}.')
-        
+
     count = like_count
     return render_template('aboutangel.html', new_count = count)
+
+@app.route('/photos', methods = ['POST','GET'])
+def picture():
+    return render_template('photos.html')
