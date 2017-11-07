d542ecb11bc0d5f342c937fc3ff564500e2a1ca0
Angel <angel@Angels-MacBook-Air.local>
2017-11-06 20:02:42 -0500

    updated form

--- flask_portfolio/portfolio/views.py
+++ flask_portfolio/portfolio/views.py
@@ -1,7 +1,12 @@
 from . import app
 from flask import Flask, render_template, request
 
+
 like_count = 0
+
+
+comments = [{'name' :'Angel','message' : 'Hello'},
+			{'name': 'Phillip', 'message': '$10 please!'}]
 
 @app.route('/', methods = ['POST','GET'])
 def index():
@@ -22,6 +27,16 @@ def about():
     count = like_count
     return render_template('aboutangel.html', new_count = count)
 
-@app.route('/photos', methods = ['POST','GET'])
-def picture():
-    return render_template('photos.html')
+@app.route('/photos', methods = ['GET', 'POST'])
+def info():
+	if request.method == 'POST':
+		name1 = request.form['name']
+		message1 = request.form['message']
+		comments.append({'name' : name1, 'message': message1})
+
+		print(comments)
+		return render_template('/photos.html', messages = comments)
+	else:
+		print('get method')
+		print(comments)
+		return render_template('photos.html', messages= comments)
